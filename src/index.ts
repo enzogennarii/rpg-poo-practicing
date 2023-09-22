0aa08592d4344933863b6e885f729581261c1901
Enzo Gennari <enzo.gennari02@gmail.com>
2023-09-22 17:41:05 -0300

    Add class PVP

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,68 @@
-// import Character from './Character';
+/* eslint-disable sonarjs/no-duplicate-string */
+import { Mage, Warrior } from './Archetypes';
+import { PVP } from './Battle';
+import Character from './Character';
+import { Orc } from './Races';
 
-// const myCharacter = new Character('Teste');
+const orcRace = new Orc('Romanos', 7);
+const warriorArchetype = new Warrior('Spartano');
+const Leonidas = new Character('Leonidas', orcRace, warriorArchetype);
 
-// console.log('name:', myCharacter.name);
-// console.log('archetype:', myCharacter.archetype);
-// console.log('defense:', myCharacter.defense);
-// console.log('dexterity:', myCharacter.dexterity);
-// console.log('energy:', myCharacter.energy);
-// console.log('lifePoints:', myCharacter.lifePoints);
-// console.log('maxLifePoints:', myCharacter.maxLifePoints);
-// console.log('race:', myCharacter.race);
-// console.log('strength:', myCharacter.strength);
+const mageArchetype = new Mage('Strange');
+const DoctorStrange = new Character('Steven Strange', orcRace, mageArchetype);
+
+// DoctorStrange.levelUp();
+// DoctorStrange.levelUp();
+// DoctorStrange.levelUp();
+
+console.log('--------------ANTES DA BATALHA---------------');
+console.log('--------------Leonidas--------------');
+console.log('name:', Leonidas.name);
+// console.log('archetype:', Leonidas.archetype);
+console.log('defense:', Leonidas.defense);
+console.log('dexterity:', Leonidas.dexterity);
+// console.log('energy:', Leonidas.energy);
+console.log('lifePoints:', Leonidas.lifePoints);
+// console.log('maxLifePoints:', Leonidas.maxLifePoints);
+// console.log('race:', Leonidas.race);
+console.log('strength:', Leonidas.strength);
+
+console.log('\n--------------Doutor Estranho--------------');
+console.log('name:', DoctorStrange.name);
+// console.log('archetype:', DoctorStrange.archetype);
+console.log('defense:', DoctorStrange.defense);
+console.log('dexterity:', DoctorStrange.dexterity);
+// console.log('energy:', DoctorStrange.energy);
+console.log('lifePoints:', DoctorStrange.lifePoints);
+// console.log('maxLifePoints:', DoctorStrange.maxLifePoints);
+// console.log('race:', DoctorStrange.race);
+console.log('strength:', DoctorStrange.strength);
+
+const result = new PVP(Leonidas, DoctorStrange).fight();
+const winner = result === 1
+  ? 'LEONIDAS VENCEU'
+  : 'DOUTOR ESTRANHO VENCEU';
+console.log('\n\n', winner);
+
+console.log('\n\n--------------DEPOIS DA BATALHA---------------');
+console.log('--------------Leonidas--------------');
+console.log('name:', Leonidas.name);
+// console.log('archetype:', Leonidas.archetype);
+console.log('defense:', Leonidas.defense);
+console.log('dexterity:', Leonidas.dexterity);
+// console.log('energy:', Leonidas.energy);
+console.log('lifePoints:', Leonidas.lifePoints);
+// console.log('maxLifePoints:', Leonidas.maxLifePoints);
+// console.log('race:', Leonidas.race);
+console.log('strength:', Leonidas.strength);
+
+console.log('\n--------------Doutor Estranho--------------');
+console.log('name:', DoctorStrange.name);
+// console.log('archetype:', DoctorStrange.archetype);
+console.log('defense:', DoctorStrange.defense);
+console.log('dexterity:', DoctorStrange.dexterity);
+// console.log('energy:', DoctorStrange.energy);
+console.log('lifePoints:', DoctorStrange.lifePoints);
+// console.log('maxLifePoints:', DoctorStrange.maxLifePoints);
+// console.log('race:', DoctorStrange.race);
+console.log('strength:', DoctorStrange.strength);
